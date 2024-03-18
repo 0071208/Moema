9ee63ecb110279574daf6aa2e5fc9e03062680d1
0071208 <162983288+0071208@users.noreply.github.com>
2024-03-18 11:35:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import pandas as pd
-voc=pd.read_CSV('lien')
+voc=pd.read_CSV('https://docs.google.com/spreadsheets/d/1jH6kJJISFA0Ye4gE4CzG7ZEjBJhCRelGOZN-HnxyZBQ/edit?pli=1#gid=0')
 st.dataframe(voc)
