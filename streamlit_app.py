563db42494972aa8a63392425309b3e43de8b741
0071208 <162983288+0071208@users.noreply.github.com>
2024-03-18 11:58:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 voc=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQLTze3TEoe78fBV0iekeHnumxiW8kWW33WjqhK65kKxll8HFbZFUGUqBwLBbdK8JZD4-z9uiOe7Bb_/pub?output=csv')
 st.dataframe(voc)
+l=voc.shape[0]
+i=np.random.choice(range(l))
+word_fr=voc['Définition'].values[i]
+word_chi=voc['pinyin'].values[i]
+st.write(word_fr+"pinyin"+word_chi)
