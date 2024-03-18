f2dcb525acef0f4934e61af40afe50f061f0f400
0071208 <162983288+0071208@users.noreply.github.com>
2024-03-18 12:05:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 voc=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQLTze3TEoe78fBV0iekeHnumxiW8kWW33WjqhK65kKxll8HFbZFUGUqBwLBbdK8JZD4-z9uiOe7Bb_/pub?output=csv')
-st.dataframe(voc)
 l=voc.shape[0]
 i=np.random.choice(range(l))
 word_fr=voc['Définition'].values[i]
